b1495545228a5758c8b41f25dfbb8797af1f4e45
Patricia Gorla <gorla.patricia@gmail.com>
2013-10-03 20:05:51 +0000

    Match patterns before packaging values.

--- app/process.py
+++ app/process.py
@@ -16,20 +16,14 @@ import pycassa
 
 import pysolr
 
-pool = ConnectionPool("solr", ["localhost:9160"])
-location_cf = ColumnFamily(pool, "location")
-person_cf = ColumnFamily(pool, "person")
-
-
 # TODO Split out tasks into threads.
 DATA_DIR = "data/"
-
+# Key-value pattern
 kv_pattern = re.compile(r'''^\<http://dbpedia.org/resource/
         (?P<name>\w+)       # Resource name
         (?!.*Date> )        # Don't match birthdate, deathdate
         .*\"(?P<value>.*)   # Value
         \"''', re.VERBOSE)
-
 # Birth and death place
 event_loc_pattern = re.compile(r'''^\<http://dbpedia.org/resource/
         (?P<name>\w+)       # Resource name
@@ -38,7 +32,6 @@ event_loc_pattern = re.compile(r'''^\<http://dbpedia.org/resource/
         .*\<http://dbpedia.org/resource/
         (?P<loc>.*)       # Location in which event occurred.
         \> .''', re.VERBOSE)
-
 # Birth and death date
 event_date_pattern = re.compile(r'''^\<http://dbpedia.org/resource/
         (?P<name>\w+)       # Resource name
@@ -48,78 +41,86 @@ event_date_pattern = re.compile(r'''^\<http://dbpedia.org/resource/
         ''', re.VERBOSE)
 
 
-def parse_life(line):
-    if event_loc_pattern.match(line):
-        vals = event_loc_pattern.search(line).groupdict()
-        name = vals['name'].replace("_", " ")
-        event = vals['event']
-        loc = vals['loc'].replace("_", " ")
-        try:
-            shortname = name.split()[0]
-        except IndexError:
-            shortname = name
-        # TODO Counter column of births, deaths
-        location_cf.insert(loc, {
-            "%s_person" % shortname: str(name)
-            })
-        person_cf.insert(name, {
-            "name": name,
-            event: loc
-            })
-
-def parse_date(line):
-    if event_date_pattern.match(line):
-        vals = event_date_pattern.search(line).groupdict()
-        name = vals['name'].replace("_", " ")
-        event = vals['event']
-        try:
-            date = datetime.strptime(vals['date'], "%Y-%m-%d")
-        except ValueError:
-            date = None
-        try:
-            shortname = name.split()[0]
-        except IndexError:
-            shortname = name
-        person_cf.insert(name, {
-            "name": name,
-            # Manually force into UTC time.
-            event: "%sZ" % date.isoformat()
-            })
-
-# Only for geo-data
-def parse_location(line):
-    if kv_pattern.match(line):
-        vals = kv_pattern.search(line).groupdict()
-        name = vals['name'].replace("_", " ")
-        print(vals)
-        location_cf.insert(
-            name, {
-            "name": name,
-            "location": vals['value']
-            })
-    pass
-
-def parse_tags(line):
-    if kv_pattern.match(line):
-        vals = kv_pattern.search(line).groupdict()
-        name = vals['name'].replace("_", " ")
-        value = str(vals['value'])
-        value_tag = "%s_tag" % random.randint(0,10)
-        person_cf.insert(
-            name, {
-            "name": name,
-            value_tag: value
-            })
-    pass
-
-def parse(line):
+def connect(fn):
     pool = ConnectionPool("solr", ["localhost:9160"])
     location_cf = ColumnFamily(pool, "location")
     person_cf = ColumnFamily(pool, "person")
 
-    for f in [parse_tags, parse_location, parse_life, parse_date]:
-        f(line)
+    batch = pycassa.batch.Mutator(pool) # default queue_size=100
 
+    for f in [parse_tags, parse_location, parse_life, parse_date]:
+        vals = f(line)
+        print(vals)
+        if vals is not None:
+            batch.insert(person_cf, *vals)
+
+    batch.send()
+
+def match_pattern(pattern):
+    """Check for pattern match before processing."""
+    def decorator(func):
+        def wrapped(line):
+            if pattern.match(line):
+                vals = pattern.search(line).groupdict()
+                for k,v in vals.items():
+                    vals[k] = str(v.replace("_", " "))
+                return func(vals)
+        return wrapped
+    return decorator
+
+
+### Person Parsers ###
+
+@match_pattern(event_loc_pattern)
+def parse_life(vals):
+    return (vals['name'], {
+        "name": vals['name'],
+        vals['event']: vals['loc']
+        })
+
+@match_pattern(event_date_pattern)
+def parse_date(vals):
+    try:
+        date = datetime.strptime(vals['date'], "%Y-%m-%d")
+    except ValueError:
+        date = None
+    try:
+        shortname = vals['name'].split()[0]
+    except IndexError:
+        shortname = vals['name']
+
+    return (vals['name'], {
+        "name": vals['name'],
+        # Manually force into UTC time.
+        vals['event']: "%sZ" % date.isoformat()
+        })
+
+@match_pattern(kv_pattern)
+def parse_tags(vals):
+    return (vals['name'], {
+        "name": vals['name'],
+        "%s_tag" % random.randint(0,10): vals['value']
+        })
+
+### Location Parsers ###
+
+@match_pattern(kv_pattern)
+def parse_location(vals):
+    return (vals['name'], {
+        "name": vals['name'],
+        "location": vals['value']
+        })
+
+@match_pattern(event_loc_pattern)
+def parse_life_location(vals):
+    try:
+        shortname = vals['name'].split()[0]
+    except IndexError:
+        shortname = vals['name']
+    # TODO Counter column of births, deaths
+    return (vals['loc'], {
+        "%s_person" % shortname: vals['name']
+        })
 
 
 if __name__ == "__main__":
@@ -129,9 +130,10 @@ if __name__ == "__main__":
         filename = sys.argv[1]
     except IndexError:
         filename = "test"
-    threadpool = Pool(20)
-    for line in open("data/geo_coordinates_en", "rb"):
-        parse_location(line)
+#    threadpool = Pool(20)
+    for line in open("data/persondata", "rb"):
+        connect(line)
+#        parse_location(line)
     #with open("%s/%s" % (DATA_DIR, filename), "rb") as fin:
     #    threadpool.map(parse, fin, 5)
     #with bz2.BZ2File("data/geo_coordinates_en.nt.bz2", "rb") as fin:

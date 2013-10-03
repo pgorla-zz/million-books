f52657e0bd886e074128749f95a817a9f2b721ad
Patricia Gorla <gorla.patricia@gmail.com>
2013-10-03 18:31:41 +0000

    Segment out function priorities.
    Reduce regex searches by collecting the entire dict of results
    in one query.

--- app/process.py
+++ app/process.py
@@ -16,6 +16,11 @@ import pycassa
 
 import pysolr
 
+pool = ConnectionPool("solr", ["localhost:9160"])
+location_cf = ColumnFamily(pool, "location")
+person_cf = ColumnFamily(pool, "person")
+
+
 # TODO Split out tasks into threads.
 DATA_DIR = "data/"
 
@@ -42,32 +47,21 @@ event_date_pattern = re.compile(r'''^\<http://dbpedia.org/resource/
         .*\"(?P<date>.*)\"  # Date
         ''', re.VERBOSE)
 
-solr = pysolr.Solr("http://localhost:8983/solr/resource/solr.person/")
-
-# Connect to 'solr' keyspace
-pool = ConnectionPool("solr", ["localhost:9160"])
-
-location_cf = ColumnFamily(pool, "location")
-person_cf = ColumnFamily(pool, "person")
-
 
 def parse_life(line):
     if event_loc_pattern.match(line):
-        name = event_loc_pattern.search(line).group('name').replace("_", " ")
-        event = event_loc_pattern.search(line).group('event')
-        loc = event_loc_pattern.search(line).group('loc').replace("_", " ")
-        print(name, event, loc)
-
+        vals = event_loc_pattern.search(line).groupdict()
+        name = vals['name'].replace("_", " ")
+        event = vals['event']
+        loc = vals['loc'].replace("_", " ")
         try:
             shortname = name.split()[0]
         except IndexError:
             shortname = name
-
         # TODO Counter column of births, deaths
         location_cf.insert(loc, {
             "%s_person" % shortname: str(name)
             })
-
         person_cf.insert(name, {
             "name": name,
             event: loc
@@ -75,45 +69,57 @@ def parse_life(line):
 
 def parse_date(line):
     if event_date_pattern.match(line):
-        name = event_date_pattern.search(line).group('name').replace("_", " ")
-        event = event_date_pattern.search(line).group('event')
-        _date = event_date_pattern.search(line).group('date')
+        vals = event_date_pattern.search(line).groupdict()
+        name = vals['name'].replace("_", " ")
+        event = vals['event']
         try:
-            date = datetime.strptime(_date, "%Y-%m-%d")
+            date = datetime.strptime(vals['date'], "%Y-%m-%d")
         except ValueError:
             date = None
-
         try:
             shortname = name.split()[0]
         except IndexError:
             shortname = name
-
         person_cf.insert(name, {
             "name": name,
             # Manually force into UTC time.
             event: "%sZ" % date.isoformat()
             })
 
+# Only for geo-data
 def parse_location(line):
     if kv_pattern.match(line):
-        name = kv_pattern.search(line).group('name').replace("_", " ")
-        location_cf.insert(name, {
+        vals = kv_pattern.search(line).groupdict()
+        name = vals['name'].replace("_", " ")
+        print(vals)
+        location_cf.insert(
+            name, {
             "name": name,
-            "location": kv_pattern.search(line).group('value')
+            "location": vals['value']
             })
     pass
 
 def parse_tags(line):
     if kv_pattern.match(line):
-        name = kv_pattern.search(line).group('name').replace("_", " ")
-        value = str(kv_pattern.search(line).group('value'))
+        vals = kv_pattern.search(line).groupdict()
+        name = vals['name'].replace("_", " ")
+        value = str(vals['value'])
         value_tag = "%s_tag" % random.randint(0,10)
-        print(name, value, value_tag)
-        person_cf.insert(name, {
+        person_cf.insert(
+            name, {
             "name": name,
             value_tag: value
             })
     pass
+
+def parse(line):
+    pool = ConnectionPool("solr", ["localhost:9160"])
+    location_cf = ColumnFamily(pool, "location")
+    person_cf = ColumnFamily(pool, "person")
+
+    for f in [parse_tags, parse_location, parse_life, parse_date]:
+        f(line)
+
 
 
 if __name__ == "__main__":
@@ -124,10 +130,9 @@ if __name__ == "__main__":
     except IndexError:
         filename = "test"
     threadpool = Pool(20)
-    #for line in bz2.BZ2File("data/persondata_en.nt.bz2", "rb"):
-    with open("%s/%s" % (DATA_DIR, filename), "rb") as fin:
-        threadpool.map(parse_tags, fin, 5)
-        threadpool.map(parse_life, fin, 5)
-        threadpool.map(parse_date, fin, 5)
+    for line in open("data/geo_coordinates_en", "rb"):
+        parse_location(line)
+    #with open("%s/%s" % (DATA_DIR, filename), "rb") as fin:
+    #    threadpool.map(parse, fin, 5)
     #with bz2.BZ2File("data/geo_coordinates_en.nt.bz2", "rb") as fin:
     #   threadpool.map(parse_location, fin, 4)
